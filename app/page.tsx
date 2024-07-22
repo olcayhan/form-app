bb1606e370192760630ed65c41b04444c070613f
olcayhan <kolcayhan1@gmail.com>
2024-07-22 12:31:16 +0300

    feat: Add dynamic rendering of forms in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,19 @@ import Link from "next/link";
 import React from "react";
 
 export default function Home() {
+  const forms = [
+    {
+      id: "1",
+      title: "Form 1",
+      description: "This is the first form",
+    },
+    {
+      id: "2",
+      title: "Form 2",
+      description: "This is the second form",
+    },
+  ];
+
   return (
     <main className="flex min-h-screen flex-row items-start justify-center p-24 gap-3 bg-neutral-100">
       <div className="w-full h-full flex flex-col items-start justify-start gap-2 p-6 border-[1px]">
@@ -14,8 +27,22 @@ export default function Home() {
             Create
           </Link>
         </div>
-        <div className="w-full flex flex-row justify-center items-center border-neutral-300 border-[1px] min-h-36 rounded-md font-semibold text-lg text-neutral-700">
-          No forms available
+        <div className="w-full flex flex-row justify-start items-center border-neutral-300 border-[1px] min-h-36 rounded-md font-semibold text-lg text-neutral-700 gap-3 p-4">
+          {forms.length === 0 ? (
+            <div className="w-full flex flex-row justify-center items-center border-neutral-300 border-[1px] min-h-36 rounded-md font-semibold text-lg text-neutral-700">
+              No forms available
+            </div>
+          ) : (
+            forms.map((form) => (
+              <Link
+                href={"form/" + form.id}
+                className="w-52 flex flex-col justify-start items-start p-6 border hover:bg-neutral-200 transition-all duration-100 rounded-md active:bg-neutral-300"
+              >
+                <h2 className="font-bold">{form.title}</h2>
+                <p>{form.description}</p>
+              </Link>
+            ))
+          )}
         </div>
       </div>
     </main>
